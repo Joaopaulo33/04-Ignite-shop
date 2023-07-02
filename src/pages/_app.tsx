dae35f324af937f386ce0334890c2aa7e4f767b0
joaopaulo33 <joaopaulo33573@gmail.com>
2023-07-02 18:48:02 -0300

    Ajustes estilização

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,8 @@
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
-
+//No _app é onde serão direcionadas a pages, ou seja todas as pages serão carregadas dentro do _app
+//O global styles fica aqui, pois não é ideal colocar css no _document
+globalStyles()
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps}  />
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ const Button = styled('button', {
 export default function Home() {
   return (
     <Button>
-      <span>Testedassd</span>
+      <span>Teste</span>
       Enviar
     </Button>
   )
