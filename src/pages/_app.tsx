7f520cc07edaa6e66fe86902cefb9dc1fa1351e8
joaopaulo33 <joaopaulo33573@gmail.com>
2023-07-26 05:41:57 -0300

    Ajuste e estilização dos produtos.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import { globalStyles } from '@/styles/global'
-import type { AppProps } from 'next/app'
-//No _app é onde serão direcionadas a pages, ou seja todas as pages serão carregadas dentro do _app
-//O global styles fica aqui, pois não é ideal colocar css no _document
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
+
+import Image from "next/image"
+
 globalStyles()
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps}  />
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <Container>
+      <Header>
+        <Image src={logoImg} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
+
+export default App
